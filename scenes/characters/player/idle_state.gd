dbcb351cad5a2d2048f635feae924cbec624ca98
KavyaJP <kavya31052005@gmail.com>
2026-01-05 21:36:52 +0530

    Added chopping, tilling, watering player state in state machine

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -1,6 +1,6 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 
 func _on_process(_delta : float) -> void:
@@ -24,7 +24,15 @@ func _on_next_transitions() -> void:
 	
 	if GameInputEvent.is_movement_input():
 		transition.emit("Walk")
-
+		
+	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvent.use_tool():
+		transition.emit("Chopping")
+	
+	if player.current_tool == DataTypes.Tools.TillGround && GameInputEvent.use_tool():
+		transition.emit("Tilling")
+		
+	if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvent.use_tool():
+		transition.emit("Watering")
 
 func _on_enter() -> void:
 	pass
---
+++ scripts/globals/data_types.gd
@@ -0,0 +1,3 @@
+class_name DataTypes
+
+enum Tools {None, AxeWood, TillGround, WaterCrops, PlantCorn, PlantTomato}
