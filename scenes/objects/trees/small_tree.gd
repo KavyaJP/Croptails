faaacd24dde4622d416104d4a96395c2b7195a0a
KavyaJP <kavya31052005@gmail.com>
2026-01-06 21:28:16 +0530

    Added a small tree with hurt-hit component and damage component

---
+++ scenes/objects/trees/small_tree.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+@onready var hurt_component: HurtComponent = $HurtComponent
+@onready var damage_component: DamageComponent = $DamageComponent
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hurt_component.hurt.connect(on_hurt)
+	damage_component.max_damage_reached.connect(on_max_damage_reached)
+
+func on_hurt(damage: int) -> void:
+	damage_component.apply_damage(damage) 
+
+func on_max_damage_reached() -> void:
+	print("Max damage reached.")
+	queue_free()
