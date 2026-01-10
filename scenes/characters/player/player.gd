9e542d3c944b5ed833aa93c7271b6ef87d66ee52
KavyaJP <kavya31052005@gmail.com>
2026-01-10 06:56:28 +0530

    Added UI panel for selecting tools

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -2,5 +2,13 @@ class_name Player
 extends CharacterBody2D
 
 @export var current_tool: DataTypes.Tools = DataTypes.Tools.None
+@onready var hit_component: HitComponent = $HitComponent
 
 var player_direction: Vector2
+
+func _ready() -> void:
+	ToolsManager.tool_selected.connect(on_tool_select)
+
+func on_tool_select(tool: DataTypes.Tools) -> void:
+	current_tool = tool
+	hit_component.current_tool = tool
